b398263790e2ab4fa3806389808d9389bdf0bdc2
Tejasbisen786 <bisentejas@gmail.com>
2023-11-09 23:06:51 +0530

    modified file acc tt router

--- L-3/server/server.js
+++ L-3/server/server.js
@@ -2,16 +2,20 @@ const express =require('express')
 
 const app=express()
 const PORT= process.env.PORT || 3000
+const router= require("./Router/auth-router")
 
-app.get('/',(req,res)=>{
-    res.status(200).send("Welcome TO Home Page")
-})
-app.get('/register',(req,res)=>{
-    res.status(200).send("Welcome TO Register Page")
-})
-app.get('/login',(req,res)=>{
-    res.status(200).send("Welcome TO LOGIN Page")
-})
+// Mount the Route
+app.use("/api/auth", router)
+
+// app.get('/',(req,res)=>{
+//     res.status(200).send("Welcome TO Home Page")
+// })
+// app.get('/register',(req,res)=>{
+//     res.status(200).send("Welcome TO Register Page")
+// })
+// app.get('/login',(req,res)=>{
+//     res.status(200).send("Welcome TO LOGIN Page")
+// })
 
 app.listen(PORT,()=>{
    console.log(`Server is running on ${PORT}`);
